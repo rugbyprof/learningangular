4272054e7bbf6c47a563f8d3d65a59866eec4ed0
rugbyprof <terry.griffin@mwsu.edu>
2014-05-15 14:41:48 -0500

    my code is better than yours

--- app/app.js
+++ app/app.js
@@ -3,17 +3,31 @@ var App = angular.module('App', []);
 var controllers = {};
 
 controllers.GetNamesController = function($scope, $http) {
-  $http.get('names.json')
+  $http.get('http://msu2u.net/~travis/getData.php')
        .then(function(res){
           $scope.names = res.data;
         });
 };
 
 controllers.GetMenuController = function($scope, $http) {
+  $scope.selectedIndex = 0;
   $http.get('menu.json')
        .then(function(res){
           $scope.menu = res.data;
         });
+
+  $scope.itemClicked = function ($index) {
+      console.log($index);
+      $scope.selectedIndex = $index;
+  }
+
 }
+
+// controllers.GetStuffFromServer = function($scop,$http){
+//   $http.get('http://msu2u.net/~travis/getData.php')
+//        .then(function(res){
+//           $scope.
+//        });
+// }
 
 App.controller(controllers);
